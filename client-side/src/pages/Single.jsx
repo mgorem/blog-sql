52285cc8f8a6906c70c0fb610a3abad0180b74da
callmemg <oremgitonga22@gmail.com>
2022-09-30 10:40:16 +0300

    add single page styles and structure

--- client-side/src/pages/Single.jsx
+++ client-side/src/pages/Single.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
+import { Link } from "react-router-dom"
+import Edit from "../images/edit.png"
+import Delete from "../images/delete.png"
 
 const Single = () => {
   return (
     <div className="single">
       <div className="content">
-        <img src="" alt="" />
+        <img src="https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
         <div className="user">
-          <img src="" alt="" />
+          <img src="https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
           <div className="info">
             <span>John</span>
             <p>Posted 2 days ago</p>
+          </div>
+          <div className="edit">
+            <Link to={`/write?edit=2`}>
+            <img src={Edit} alt="edit" />
+            </Link>
+            <img src={Delete} alt="delete" />
           </div>
         </div>
       </div>
